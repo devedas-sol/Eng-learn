c5ab1cb2fcba852751782f5802a5ae52287b354e
devedas-sol <soldeve9@gmail.com>
2025-08-22 14:03:13 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Politik Lern-App</h1>
+      <p>Willkommen! Bitte ersetzen Sie diesen Text durch Ihre App-Komponenten.</p>
+    </div>
+  );
+}
